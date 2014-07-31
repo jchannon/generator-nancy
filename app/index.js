94ea4ab0c573f50a01d594e6500f96e1add72a89
Jonathan Channon <jonathan.channon@gmail.com>
2014-07-31 15:58:27 +0000

    0.0.3

--- app/index.js
+++ app/index.js
@@ -11,36 +11,36 @@ var AspnetGenerator = yeoman.generators.Base.extend({
         this.log(yosay('Welcome to the marvelous Nancy generator!'));
     },
 
-    askFor: function () {
-        var done = this.async();
+    // askFor: function () {
+    //     var done = this.async();
 
-        var prompts = [{
-            type: 'list',
-            name: 'type',
-            message: 'What type of application do you want to create?',
-            choices: [
-            {
-                name: 'Console Application',
-                value: 'console'
-            },
-            {
-                name: 'Web Application',
-                value: 'web'
-            }
-            ]
-        }];
+    //     // var prompts = [{
+    //     //     type: 'list',
+    //     //     name: 'type',
+    //     //     message: 'What type of application do you want to create?',
+    //     //     choices: [
+    //     //     {
+    //     //         name: 'Console Application',
+    //     //         value: 'console'
+    //     //     },
+    //     //     {
+    //     //         name: 'Web Application',
+    //     //         value: 'web'
+    //     //     }
+    //     //     ]
+    //     // }];
 
-        this.prompt(prompts, function (props) {
-            this.type = props.type;
+    //     // this.prompt(prompts, function (props) {
+    //     //     this.type = props.type;
 
-            done();
-            }.bind(this));
-        },
+    //     //     done();
+    //     //     }.bind(this));
+    //     },
 
     askForName: function () {
         var done = this.async();
 
-        var app = (this.type == 'console'? 'ConsoleApplication1' : 'WebApplication1')
+        var app = 'MyNancyApp';
         var prompts = [{
             name: 'applicationName',
             message: 'What\'s the name of your Nancy ASP.NET application?',
@@ -65,14 +65,8 @@ var AspnetGenerator = yeoman.generators.Base.extend({
         this.copy(this.cacheRoot() + '/jchannon/aspnet_vnext_samples/master/NuGet.config', 'NuGet.config');
         this.mkdir(this.applicationName);
         switch(this.type) {
-            case 'console':
-                this.directory(this.cacheRoot() + '/jchannon/aspnet_vnext_samples/master/console', this.applicationName);
-                break;
             case 'web':
                 this.directory(this.cacheRoot() + '/jchannon/aspnet_vnext_samples/master/web', this.applicationName);
-                break;
-            case 'mvc':
-                this.directory(this.cacheRoot() + '/jchannon/aspnet_vnext_samples/master/mvc', this.applicationName);
                 break;
             default:
                 console.log('Unimplemented');
